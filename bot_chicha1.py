8d0ce86b543916f2a4949e328d4904c5014ddf65
Masami2783 <88360032+Masami2783@users.noreply.github.com>
2024-08-09 08:43:20 +0500

    second

---
+++ bot_chicha1.py
@@ -0,0 +1,28 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # URL вашего Web App
+    web_app_url = "https://masami2783.github.io/"
+
+    # Создание кнопки, открывающей Web App
+    keyboard = [
+        [InlineKeyboardButton("Open Web App", web_app=web_app_url)],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # Отправка сообщения с кнопкой
+    await update.message.reply_text("Click the button below to open the Web App.", reply_markup=reply_markup)
+
+def main() -> None:
+    # Введите сюда токен вашего бота
+    application = Application.builder().token("7284584129:AAFNm1mzoIV3OrJ0QmfJ3PoxPvV61TJe7pk").build()
+
+    # Добавление обработчика команды
+    application.add_handler(CommandHandler("start", start))
+
+    # Запуск бота
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
